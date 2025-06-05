2486bf53923fb8033219c6a3d786639a2240fe45
feranmiola <osunjuyigbeiyin@gmail.com>
2025-06-05 19:54:08 +0100

    Add custom fonts and clean up page component

---
+++ src/Components/Topbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <div className="flex items-center justify-between flex-row w-full p-5"></div>
+  );
+};
+
+export default Topbar;
