e2cc76a60ba43b66f1079a2081a03b0634a12492
feranmiola <osunjuyigbeiyin@gmail.com>
2025-06-05 23:18:29 +0100

    Implement custom scrollbar behavior and update body background color for improved aesthetics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,48 @@
+"use client";
 import Hero from "@/Components/Hero";
 import Topbar from "@/Components/Topbar";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    let scrollTimer = 0;
+
+    function updateScrollbar() {
+      const scrollPercentage =
+        window.scrollY /
+        (document.documentElement.scrollHeight - window.innerHeight);
+      const scrollbarHeight =
+        (window.innerHeight / document.documentElement.scrollHeight) *
+        window.innerHeight;
+      const scrollTop =
+        scrollPercentage * (window.innerHeight - scrollbarHeight);
+
+      document.body.style.setProperty("--scroll-top", `${scrollTop}px`);
+      document.body.style.setProperty(
+        "--scrollbar-height",
+        `${scrollbarHeight}px`
+      );
+
+      document.body.classList.add("is-scrolling");
+
+      clearTimeout(scrollTimer);
+      // @ts-ignore
+      scrollTimer = setTimeout(() => {
+        document.body.classList.remove("is-scrolling");
+      }, 1000);
+    }
+    window.addEventListener("scroll", updateScrollbar);
+    window.addEventListener("resize", updateScrollbar);
+    updateScrollbar();
+
+    return () => {
+      window.removeEventListener("scroll", updateScrollbar);
+      window.removeEventListener("resize", updateScrollbar);
+    };
+  }, []);
+
   return (
     <div className="bg-[#010010] flex flex-col w-full min-h-screen">
       <Topbar />
