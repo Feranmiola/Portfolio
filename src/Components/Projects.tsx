26a7461abb28604431745ac9a42ea57f11178e95
feranmiola <osunjuyigbeiyin@gmail.com>
2025-06-06 14:09:44 +0100

    Add detailed project showcase in Projects component, including Nefesol description and buttons for project viewing; update ArrowRight icon to accept color props for better customization.

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ArrowRight from "./Icons/ArrowRight";
 
 const Projects = () => {
   return (
@@ -15,7 +16,55 @@ const Projects = () => {
           </p>
         </div>
 
-        <div></div>
+        <div className="fles w-full flex-col space-y-10">
+          <div className="w-full p-5 items-center bg-[#151431] rounded-[12px] border-[0.5px] border-[#515151] flex flex-row justify-between">
+            <div className="w-[636px] h-[382px]">
+              <img
+                src="https://res.cloudinary.com/debiu7z1b/image/upload/v1749214380/Frame_15_zsz9gs.webp"
+                className="w-full h-full"
+                alt="Nefesol"
+              />
+            </div>
+
+            <div className="flex flex-col w-[484px] h-[423px] justify-between">
+              <div className="flex flex-col space-y-1">
+                <p className="font-merriweather text-white text-[32px]">
+                  Nefesol
+                </p>
+                <p className="font-poppins text-[#CCCCCC] text-base">
+                  A carbon offset platform enabling users to plant trees and
+                  receive certificates for their climate contributions. Features
+                  multi-location support, PayPal integration, and automated
+                  email flows.
+                </p>
+              </div>
+
+              <div className="flex flex-col space-y-3">
+                <p className="font-merriweather text-white text-[20px] px-5 py-2 border border-[#525252] rounded-[100px] w-max">
+                  Technologies used
+                </p>
+                <p className="font-merriweather text-white text-[20px] px-5 py-2 border border-[#525252] rounded-[100px] w-max">
+                  Technologies used
+                </p>
+              </div>
+
+              <div className="flex flex-row items-center space-x-5  ">
+                <div className="flex flex-row items-center bg-white rounded-[100px] w-[236px] h-[54px] justify-center space-x-2 cursor-pointer">
+                  <p className="font-poppins font-semibold text-[#1E1E1E] text-[16px]">
+                    View Projets
+                  </p>
+                </div>
+
+                <div className="w-[221px] h-[54px] flex items-center justify-center flex-row space-x-2  border border-white rounded-[100px] text-[18px] font-semibold bg-transparent text-white transition-all ease-in-out cursor-pointer">
+                  <p className="font-poppins font-semibold text-white text-[16px]">
+                    View Projets
+                  </p>{" "}
+                  <ArrowRight color="white" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
