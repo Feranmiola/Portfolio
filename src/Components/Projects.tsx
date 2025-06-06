1ad59f5a1cd8bea4a3302cc7aec46227502da582
feranmiola <osunjuyigbeiyin@gmail.com>
2025-06-06 14:50:51 +0100

    Add Akeso Health and Stepverse project showcases to Projects component, including descriptions, technology icons, and interactive buttons for project viewing.

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -116,6 +116,100 @@ const Projects = () => {
               />
             </div>
           </div>
+
+          <div className="w-full p-5 items-center bg-[#151431] rounded-[12px] border-[0.5px] border-[#515151] flex flex-row justify-between">
+            <div className="w-[636px] h-[382px]">
+              <img
+                src="https://res.cloudinary.com/debiu7z1b/image/upload/v1749214380/Frame_15_1_dmxv1c.webp"
+                className="w-full h-full"
+                alt="Akeso"
+              />
+            </div>
+
+            <div className="flex flex-col w-[484px] h-[423px] justify-between py-3">
+              <div className="flex flex-col space-y-1">
+                <p className="font-merriweather text-white text-[32px]">
+                  Akeso Health
+                </p>
+                <p className="font-poppins text-[#CCCCCC] text-base">
+                  A responsive web platform for connected patient care, designed
+                  to streamline communication and reduce overhead for healthcare
+                  providers.
+                </p>
+              </div>
+
+              <div className="flex flex-col space-y-3">
+                <p className="font-merriweather text-white text-[20px] px-5 py-2 border border-[#525252] rounded-[100px] w-max">
+                  Technologies used
+                </p>
+                <div className="flex flex-row items-center space-x-4">
+                  <Viteicon />
+                </div>
+              </div>
+
+              <div className="flex flex-row items-center space-x-5  ">
+                <div className="flex flex-row items-center bg-white rounded-[100px] w-[236px] h-[54px] justify-center space-x-2 cursor-pointer">
+                  <p className="font-poppins font-semibold text-[#1E1E1E] text-[16px]">
+                    View Projets
+                  </p>
+                </div>
+
+                <div className="w-[221px] h-[54px] flex items-center justify-center flex-row space-x-2  border border-white rounded-[100px] text-[18px] font-semibold bg-transparent text-white transition-all ease-in-out cursor-pointer">
+                  <p className="font-poppins font-semibold text-white text-[16px]">
+                    View Projets
+                  </p>{" "}
+                  <ArrowRight color="white" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full p-5 items-center bg-[#151431] rounded-[12px] border-[0.5px] border-[#515151] flex flex-row justify-between">
+            <div className="flex flex-col w-[484px] h-[423px] justify-between py-3">
+              <div className="flex flex-col space-y-1">
+                <p className="font-merriweather text-white text-[32px]">
+                  Stepverse
+                </p>
+                <p className="font-poppins text-[#CCCCCC] text-base">
+                  Web3 fitness game using Telegram Mini Apps. Features include
+                  leaderboards, rewards, and community tracking.
+                </p>
+              </div>
+
+              <div className="flex flex-col space-y-3">
+                <p className="font-merriweather text-white text-[20px] px-5 py-2 border border-[#525252] rounded-[100px] w-max">
+                  Technologies used
+                </p>
+                <div className="flex flex-row items-center space-x-4">
+                  <NextJsIcon />
+                  <Viteicon />
+                </div>
+              </div>
+
+              <div className="flex flex-row items-center space-x-5  ">
+                <div className="flex flex-row items-center bg-white rounded-[100px] w-[236px] h-[54px] justify-center space-x-2 cursor-pointer">
+                  <p className="font-poppins font-semibold text-[#1E1E1E] text-[16px]">
+                    View Projets
+                  </p>
+                </div>
+
+                <div className="w-[221px] h-[54px] flex items-center justify-center flex-row space-x-2  border border-white rounded-[100px] text-[18px] font-semibold bg-transparent text-white transition-all ease-in-out cursor-pointer">
+                  <p className="font-poppins font-semibold text-white text-[16px]">
+                    View Projets
+                  </p>{" "}
+                  <ArrowRight color="white" />
+                </div>
+              </div>
+            </div>
+
+            <div className="w-[636px] h-[382px]">
+              <img
+                src="https://res.cloudinary.com/debiu7z1b/image/upload/v1749214379/Frame_15_3_howzqk.webp"
+                className="w-full h-full"
+                alt="Stepverse"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
