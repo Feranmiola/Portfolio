53016359faa810a18f28350641bfbbf74856be05
feranmiola <osunjuyigbeiyin@gmail.com>
2025-06-06 14:52:52 +0100

    Add Webmacht project showcase to Projects component, including project description, technology icons, and interactive buttons for project viewing; enhance layout for better user engagement.

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -57,6 +57,7 @@ const Projects = () => {
                   <p className="font-poppins font-semibold text-[#1E1E1E] text-[16px]">
                     View Projets
                   </p>
+                  <ArrowRight />
                 </div>
 
                 <div className="w-[221px] h-[54px] flex items-center justify-center flex-row space-x-2  border border-white rounded-[100px] text-[18px] font-semibold bg-transparent text-white transition-all ease-in-out cursor-pointer">
@@ -97,6 +98,7 @@ const Projects = () => {
                   <p className="font-poppins font-semibold text-[#1E1E1E] text-[16px]">
                     View Projets
                   </p>
+                  <ArrowRight />
                 </div>
 
                 <div className="w-[221px] h-[54px] flex items-center justify-center flex-row space-x-2  border border-white rounded-[100px] text-[18px] font-semibold bg-transparent text-white transition-all ease-in-out cursor-pointer">
@@ -152,6 +154,7 @@ const Projects = () => {
                   <p className="font-poppins font-semibold text-[#1E1E1E] text-[16px]">
                     View Projets
                   </p>
+                  <ArrowRight />
                 </div>
 
                 <div className="w-[221px] h-[54px] flex items-center justify-center flex-row space-x-2  border border-white rounded-[100px] text-[18px] font-semibold bg-transparent text-white transition-all ease-in-out cursor-pointer">
@@ -191,6 +194,7 @@ const Projects = () => {
                   <p className="font-poppins font-semibold text-[#1E1E1E] text-[16px]">
                     View Projets
                   </p>
+                  <ArrowRight />
                 </div>
 
                 <div className="w-[221px] h-[54px] flex items-center justify-center flex-row space-x-2  border border-white rounded-[100px] text-[18px] font-semibold bg-transparent text-white transition-all ease-in-out cursor-pointer">
@@ -208,6 +212,52 @@ const Projects = () => {
                 className="w-full h-full"
                 alt="Stepverse"
               />
+            </div>
+          </div>
+
+          <div className="w-full p-5 items-center bg-[#151431] rounded-[12px] border-[0.5px] border-[#515151] flex flex-row justify-between">
+            <div className="w-[636px] h-[382px]">
+              <img
+                src="https://res.cloudinary.com/debiu7z1b/image/upload/v1749214380/Frame_16_h4swtl.webp"
+                className="w-full h-full"
+                alt="Webmacht"
+              />
+            </div>
+            <div className="flex flex-col w-[484px] h-[423px] justify-between py-3">
+              <div className="flex flex-col space-y-1">
+                <p className="font-merriweather text-white text-[32px]">
+                  Webmacht
+                </p>
+                <p className="font-poppins text-[#CCCCCC] text-base">
+                  Worked across sectors to deliver tailored platforms — from
+                  HIPAA-compliant portals to virtual real estate tools.
+                </p>
+              </div>
+
+              <div className="flex flex-col space-y-3">
+                <p className="font-merriweather text-white text-[20px] px-5 py-2 border border-[#525252] rounded-[100px] w-max">
+                  Technologies used
+                </p>
+                <div className="flex flex-row items-center space-x-4">
+                  <NextJsIcon />
+                </div>
+              </div>
+
+              <div className="flex flex-row items-center space-x-5  ">
+                <div className="flex flex-row items-center bg-white rounded-[100px] w-[236px] h-[54px] justify-center space-x-2 cursor-pointer">
+                  <p className="font-poppins font-semibold text-[#1E1E1E] text-[16px]">
+                    View Projets
+                  </p>
+                  <ArrowRight />
+                </div>
+
+                <div className="w-[221px] h-[54px] flex items-center justify-center flex-row space-x-2  border border-white rounded-[100px] text-[18px] font-semibold bg-transparent text-white transition-all ease-in-out cursor-pointer">
+                  <p className="font-poppins font-semibold text-white text-[16px]">
+                    View Projets
+                  </p>{" "}
+                  <ArrowRight color="white" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
